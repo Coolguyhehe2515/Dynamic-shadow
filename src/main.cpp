46a1a00323b41fbb4f168ed1efdb30a210cbe770
Coolguy <mobilemc022@gmail.com>
2026-09-09 13:48:03 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,8 @@
+#include "DynamicShadowsMod.h"
+
 #include <pl/Mod.hpp>
-
-#include "render/RenderHooks.hpp"
-
-class DynamicShadowsMod : public ll::mod::NativeMod {
-public:
-    DynamicShadowsMod()
-        : NativeMod(*NativeMod::current()) {}
-
-    bool load() override {
-        return true;
-    }
-
-    bool enable() override {
-        if (!dynamicshadows::render::install()) {
-            return false;
-        }
-
-        return true;
-    }
-
-    bool disable() override {
-        dynamicshadows::render::uninstall();
-        return true;
-    }
-
-    bool unload() override {
-        dynamicshadows::render::uninstall();
-        return true;
-    }
-};
 
 PL_REGISTER_MOD(
     DynamicShadowsMod,
-    DynamicShadowsMod()
+    DynamicShadowsMod::instance()
 );
