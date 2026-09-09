83039d591843a3310f6d90da65868b8ce2c98580
Coolguy <mobilemc022@gmail.com>
2026-09-09 12:17:38 +0700

    Implement DynamicShadowsMod for rendering

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <pl/Mod.hpp>
+
+#include "render/RenderHooks.hpp"
+
+class DynamicShadowsMod : public ll::mod::NativeMod {
+public:
+    DynamicShadowsMod() : NativeMod(*ll::mod::NativeMod::current()) {}
+
+    bool load() override {
+        getSelf().getLogger().info("DynamicShadows loading...");
+        return true;
+    }
+
+    bool enable() override {
+        getSelf().getLogger().info("DynamicShadows enabling...");
+
+        if (!dynamicshadows::render::install()) {
+            getSelf().getLogger().error("Failed to install RenderLevel hook.");
+            return false;
+        }
+
+        return true;
+    }
+
+    bool disable() override {
+        dynamicshadows::render::uninstall();
+        getSelf().getLogger().info("DynamicShadows disabled.");
+        return true;
+    }
+
+    bool unload() override {
+        dynamicshadows::render::uninstall();
+        return true;
+    }
+};
+
+PL_REGISTER_MOD(DynamicShadowsMod);
